769e5c5195795da8284d92238c4521dc3f932f1e
jaggi47 <iamjagmohansingh17@gmail.com>
2022-11-07 13:46:45 +0530

    OT-013: fix country code

--- src/routes/CreateSpecialist/index.tsx
+++ src/routes/CreateSpecialist/index.tsx
@@ -7,7 +7,7 @@ import { API } from "../../constants";
 import { BsChevronLeft } from "react-icons/bs";
 import { IoImageOutline } from "react-icons/io5";
 import PhoneInput from "react-phone-input-2";
-
+import "react-phone-input-2/lib/plain.css";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { useRef, useState } from "react";
 import { Create } from "../../api/Create";
@@ -26,6 +26,7 @@ const CreateSpecialist = () => {
     sendNotifications: false,
   });
   const [phone, setPhone] = useState("");
+  const [phoneCode, setPhoneCode] = useState("");
 
   const [selectedImage, setSelectedImage] = useState("");
 
@@ -55,8 +56,9 @@ const CreateSpecialist = () => {
     const data = {
       specialistEmail: getInputValue(emailRef),
       name: getInputValue(nameRef),
-      phoneNumber: getInputValue(phoneRef),
+      phoneNumber: phone,
       access: permissions,
+      phoneCode,
     };
 
     if (!selectedImage) {
@@ -121,31 +123,29 @@ const CreateSpecialist = () => {
             }}
           />
 
+          <div className="field-heading">Contact Number</div>
           <PhoneInput
             inputProps={{
               name: "Phone Number",
               required: true,
               autoFocus: true,
             }}
-            country="us"
-            value={phone}
+            country={"us"}
+            value={phoneCode+phone}
+            onKeyDown={(val: any) => {
+              setPhoneCode(val.target.value.split(" ")[0])
+              setPhone(val.target.value.split(" ").slice(1).join(""))
+            }}
             specialLabel="Phone Number"
             inputClass="field-value"
             containerClass="input-tel"
             buttonClass="flag-dropdown"
-            onChange={(value: any) => setPhone(value)}
+            // onChange={(value: any) => 
+            //   {
+            //     console.log({value})
+            //     setPhone(value)
+            //   }}
           />
-
-          {/* <InputForm
-            inputFields={{
-              placeholder: "12345678",
-              ref: phoneRef,
-              name: "Phone Number",
-              id: "tel",
-              maxlength: 30,
-              type: "tel",
-            }}
-          /> */}
 
           <button className="button-submit-itinerary" type="submit">
             Create
