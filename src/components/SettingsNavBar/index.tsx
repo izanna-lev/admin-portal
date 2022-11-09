2f746282bc5cea680e62fcd3380aa86e80cde5b9
Shivender-Kun <shivenderkumar761@gmail.com>
2022-11-09 11:23:57 +0530

    assets import changed

--- src/components/SettingsNavBar/index.tsx
+++ src/components/SettingsNavBar/index.tsx
@@ -1,5 +1,5 @@
 import NavigationOption from "../sub-components/NavigationOption";
-import { ICON } from "../../constants";
+import { ICON } from "../../assets/index";
 import styles from "./index.module.scss";
 
 const SettingsNavBar = ({ sideNavView }: { sideNavView: boolean }) => (
