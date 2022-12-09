f563369ae2e8fdba8969744c98acc653fc232880
Shivender-Kun <shivenderkumar761@gmail.com>
2022-12-09 15:07:38 +0530

    chat

--- src/store/Actions/chat.tsx
+++ src/store/Actions/chat.tsx
@@ -8,24 +8,33 @@ import { setLoader } from "../slices/loader";
 import { setApiMessage } from "../slices/apiMessage";
 import { getChat } from "../slices/chatList";
 
-export const chatList = (): ThunkAction<void, RootState, unknown, AnyAction> => {
+export const chatList = (): ThunkAction<
+  void,
+  RootState,
+  unknown,
+  AnyAction
+> => {
   return async (
     dispatch: (arg0: { payload: any; type: string }) => void,
     getState: any
   ) => {
     try {
-      const Authorization = localStorage.getItem("accessToken") || ""
+      const Authorization = localStorage.getItem("accessToken") || "";
       dispatch(setLoader(true));
-      const response = await axios.post(API.CHAT_LIST, {}, {
-        headers: {
-          Authorization
-        },
-      });
+      const response = await axios.post(
+        API.CHAT_LIST,
+        {},
+        {
+          headers: {
+            Authorization,
+          },
+        }
+      );
       dispatch(setLoader(false));
       if (response.data.code !== 100) throw new Error(response.data.message);
       dispatch(getChat(response.data));
     } catch (err: any) {
-        dispatch(setLoader(false));
+      dispatch(setLoader(false));
       dispatch(
         setApiMessage({
           data: {
