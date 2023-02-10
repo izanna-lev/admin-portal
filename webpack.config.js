f9e7dac24b4820c8fe08b3913f1b804090c0eb06
Shivender-Kun <shivenderkumar761@gmail.com>
2023-02-10 14:53:38 +0530

    google places api key updated

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ const BUILD_DIR = path.resolve(__dirname, "build");
 //   BRANCH: "development",
 //   S3_URL: "https://app-onsite.s3.amazonaws.com/",
 //   SERVER: "https://developmentapi.onsiteplanning.com/",
-//   GOOGLE_API: "AIzaSyByy1LrT-5ZQ642PzXM4m_WCQ-fS6GO-9s",
+//   GOOGLE_API: "AIzaSyAOO-vhn-vejr178fuNVujksicv8nsTGfQ",
 // };
 
 module.exports = (env) => {
